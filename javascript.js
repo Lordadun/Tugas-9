42ec715bfbd0455e27481ad7cd37865577ae6b7a
Lordadun <r.reihan1923@gmail.com>
2025-05-14 04:19:24 +0700

    first commit

--- javascript.js
+++ javascript.js
@@ -1,16 +1,57 @@
-const scrollBtn = document.getElementById("scrollBtn");
+$(document).ready(function () {
+  // Munculkan tombol jika scroll > 100px
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $("#scrollToTop").fadeIn();
+    } else {
+      $("#scrollToTop").fadeOut();
+    }
+  });
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    scrollBtn.style.display = "block";
-  } else {
-    scrollBtn.style.display = "none";
-  }
+  // Saat diklik, scroll ke atas
+  $("#scrollToTop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
 });
 
-scrollBtn.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
+$(document).ready(function () {
+  $(".scroll-link").click(function (e) {
+    e.preventDefault(); // Mencegah aksi default anchor
+    var target = $(this).attr("href");
+
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top,
+      },
+      800
+    ); // 800ms = durasi animasi
   });
+});
+
+ScrollReveal().reveal(".card-home-reveal", {
+  distance: "50px",
+  duration: 1000,
+  easing: "ease-in-out",
+  origin: "bottom",
+  interval: 200,
+  reset: true,
+});
+
+ScrollReveal().reveal(".card-event-reveal", {
+  distance: "50px",
+  duration: 1000,
+  easing: "ease-in-out",
+  origin: "bottom",
+  interval: 200,
+  reset: true,
+});
+
+ScrollReveal().reveal(".card-gallery-reveal", {
+  distance: "50px",
+  duration: 1000,
+  easing: "ease-in-out",
+  origin: "bottom",
+  interval: 200,
+  reset: true,
 });
